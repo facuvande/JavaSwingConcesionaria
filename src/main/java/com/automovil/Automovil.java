fafff337115326c3686a4d00abd288186a137f61
Facundo <facundovs84@hotmail.com.ar>
2023-11-09 12:07:16 -0300

    Creada interfaz grafica

--- src/main/java/com/automovil/Automovil.java
+++ src/main/java/com/automovil/Automovil.java
@@ -1,8 +1,16 @@
 package com.automovil;
 
+import com.automovil.igu.Principal;
+
 public class Automovil {
 
     public static void main(String[] args) {
+        // Llama a la Interfaz Grafica
+        Principal miPantalla = new Principal();
+        
+        miPantalla.setVisible(true);
+        miPantalla.setLocationRelativeTo(null);
+                
         
     }
 }
